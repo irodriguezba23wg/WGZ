bf281e16d1b2151d2f98ec76eb3d96d94335119e
Iker <irodriguezba23wg@ikzubirimanteo.com>
2024-11-19 23:09:58 +0100

    indexean aldaketak egiten

--- src/kotxe_jabe/views/Kotxeakbistaratu.php
+++ src/kotxe_jabe/views/Kotxeakbistaratu.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kotxeak eta Jabeak</title>
 </head>
 <body>
     <h1>Kotxeen Lista</h1>
-    <?php if (!empty($lista)): ?>
-        <?php foreach ($lista as $fila): ?>
+
+    <?php if (!empty($kotxeak)): ?>
+        <?php foreach ($kotxeak as $kotxea): ?>
             <form method="GET" action="../public/JabeaIdAldatu.php">
             <ul>
                 <!-- Campo oculto para kotxe_id siempre presente -->
-                <input type="hidden" name="kotxe_id" value="<?= htmlspecialchars($fila['id']) ?>">
+                <input type="hidden" name="kotxe_id" value="<?= htmlspecialchars($kotxea['id']) ?>">
 
-                <li>ID Kotxe: <?= htmlspecialchars($fila['id']) ?></li>
-                <?php if ($fila['jabea_id'] != NULL): ?>
-                    <li>ID Jabea: <?= htmlspecialchars($fila['jabea_id']) ?></li><button formaction="borratuJabea.php">Borratu Jabea</button>
+                <li>ID Kotxe: <?= htmlspecialchars($kotxea['id']) ?></li>
+                
+                <?php if ($kotxea['jabea_id'] != NULL): ?>
+                    <li>ID Jabea: <?= htmlspecialchars($kotxea['jabea_id']) ?></li>
+                    <button formaction="borratuJabea.php">Borratu Jabea</button>
                 <?php else: ?>
                     <li>
                         <label style="color: red;">Ez dauka jaberik, Autatu bat:</label>
                         <select name="jabea_id">
-                            <?php if (!empty($listaJabe)): ?>
-                                <?php foreach ($listaJabe as $filaJabe): ?>
-                                    <option value="<?= htmlspecialchars($filaJabe['id']) ?>">
-                                        <?= htmlspecialchars($filaJabe['izena']) ?>
+                            <?php if (!empty($jabeak)): ?>
+                                <?php foreach ($jabeak as $jabea): ?>
+                                    <option value="<?= htmlspecialchars($jabea['id']) ?>">
+                                        <?= htmlspecialchars($jabea['izena']) ?>
                                     </option>
                                 <?php endforeach; ?>
                             <?php else: ?>
@@ -37,37 +40,46 @@
                         <button type="submit">Sartu Jabea</button>
                     </li>
                 <?php endif; ?>
-                <li>Matrikula: <?= htmlspecialchars($fila['matrikula']) ?></li>
-                <li>Matrikulazio Data: <?= htmlspecialchars($fila['matrikulazio_data']) ?></li>
-                <li>ITV: <?= htmlspecialchars($fila['itv']) ?></li>
+
+                <li>Matrikula: <?= htmlspecialchars($kotxea['matrikula']) ?></li>
+                <li>Matrikulazio Data: <?= htmlspecialchars($kotxea['matrikulazio_data']) ?></li>
+                <li>ITV: <?= htmlspecialchars($kotxea['itv']) ?></li>
             </ul>
             <button formaction="Kotxeadelete.php">Kotxea Ezabatu</button>
             </form>
             <hr>
         <?php endforeach; ?>
     <?php endif; ?>
-    <H1>Kotxe berri bat sartu</H1>
-    <form method="GET">
+
+    <h1>Kotxe berri bat sartu</h1>
+    <form method="POST" action="insertKotxe.php">
         <label>Jabea:</label>
-        <select name="jabea">
+        <select name="jabea_id">
             <option value="NULL">Ez dauka jaberik</option>
-            <?php if (!empty($listaJabe)): ?>
-                <?php foreach ($listaJabe as $filaJabe): ?>
-                    <option value="<?= htmlspecialchars($filaJabe['id']) ?>">
-                        <?= htmlspecialchars($filaJabe['izena']) ?>
+            <?php if (!empty($jabeak)): ?>
+                <?php foreach ($jabeak as $jabea): ?>
+                    <option value="<?= htmlspecialchars($jabea['id']) ?>">
+                        <?= htmlspecialchars($jabea['izena']) ?>
                     </option>
                 <?php endforeach; ?>
             <?php else: ?>
                 <option>No hay datos disponibles</option>
             <?php endif; ?>
         </select>
+
         <label>Matrikula:</label>
-        <input type="text" name="matrikula" value="" minlength="7">
+        <input type="text" name="matrikula" minlength="7" required>
+
         <label>Matrikulazio Data:</label>
-        <input type="text" name="matrikula_data" value="">
+        <input type="date" name="matrikula_data" required>
+
         <label>ITV:</label>
         <select name="itv">
             <option value="1">Bai</option>
             <option value="0">Ez</option>
         </select>
+
+        <button type="submit">Gorde Kotxea</button>
     </form>
+</body>
+</html>
